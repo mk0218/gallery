a41c3b1d01f9843a95dbfda56a5b1b6c73d5e63a
Kim Minkyoung <mk0218@postech.ac.kr>
2025-03-02 03:31:34 +0900

    Edited databasse.t

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,9 +1,7 @@
 import { neon } from '@neondatabase/serverless';
 
-const DATABASE_URL = process.env.DATABASE_URL ?? '';
-
 export async function getCategories() {
-	const sql = neon(DATABASE_URL);
+	const sql = neon(process.env.DATABASE_URL!);
 	const result = await sql`SELECT DISTINCT(category) FROM images;`;
 	return result.map((r) => r.category);
 }
