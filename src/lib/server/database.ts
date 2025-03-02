5608b4b41df70fadc4c37a6fe42207eef43cbd3a
Minkyoung Kim <minkyoung.kim@alsemy.com>
2025-03-02 11:02:48 +0900

    Change the way env variables are fetched.

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,7 +1,14 @@
+import { DATABASE_URL } from '$env/static/private';
 import { neon } from '@neondatabase/serverless';
 
 export async function getCategories() {
-	const sql = neon(process.env.DATABASE_URL!);
+	const sql = neon(DATABASE_URL);
 	const result = await sql`SELECT DISTINCT(category) FROM images;`;
 	return result.map((r) => r.category);
+}
+
+export async function getImagesByCategories(category: string) {
+	const sql = neon(DATABASE_URL);
+	const result = await sql`SELECT key FROM images WHERE category = ${category}`;
+	return result.map((r) => r.key);
 }
